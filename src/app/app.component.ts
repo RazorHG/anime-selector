a85cc8608489de9c8de566c785485eef3ef6d2f2
Kevin Barcelon <boundtogo@gmail.com>
2025-02-19 18:53:00 -0500

    update styles. use form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Seasons } from './core/seasons';
 import { AnimeBase, MyAnimeListService } from './core/myanimelist.service';
 import { TitleCasePipe } from '@angular/common';
@@ -21,15 +21,22 @@ import { TitleCasePipe } from '@angular/common';
     MatFormFieldModule,
     MatButtonModule,
     TitleCasePipe,
+    ReactiveFormsModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   private myAnimeListService = inject(MyAnimeListService);
-  title = 'anime-selector';
-  selectedYear!: number;
-  selectedSeason!: Seasons;
+
+  queryForm = new FormGroup({
+    selectedYear: new FormControl<number>(2025, Validators.required),
+    selectedSeason: new FormControl<Seasons>(Seasons.spring, Validators.required),
+    willWatch: new FormControl<AnimeBase[]>([]),
+    wontWatch: new FormControl<AnimeBase[]>([]),
+    updates: new FormControl<AnimeBase[]>([], Validators.minLength(1)),
+  });
+
   animeList: Array<AnimeBase> = [];
 
   seasons = [
@@ -44,17 +51,33 @@ export class AppComponent {
   ];
 
   loadAnime() {
-    this.selectedYear
-    this.myAnimeListService.getSeasonalAnime(this.selectedYear, this.selectedSeason).subscribe((response) => {
-      const animeList = response.data.map((node) => {
-        return node.node
+    const selectedYear = this.queryForm.get('selectedYear')?.value;
+    const selectedSeason = this.queryForm.get('selectedSeason')?.value;
+    console.log(selectedYear, selectedSeason);
+    if (selectedYear && selectedSeason) {
+      this.myAnimeListService.getSeasonalAnime(selectedYear, selectedSeason).subscribe((response) => {
+        const animeList = response.data.map((node) => {
+          return node.node
+        })
+        // filter out already sorted anime
+        this.animeList = animeList.filter((anime) => {
+          return !(this.queryForm.get('updates')?.value as AnimeBase[]).find((updatedAnime) => { return updatedAnime.id === anime.id; })
+        });
+        this.queryForm.updateValueAndValidity();
       })
-      this.animeList = animeList;
-    })
+    }
+  }
+
+  saveAnime() {
+    console.log(this.queryForm.get('updates')?.valid);
   }
 
   updateSortedAnime(event: SortedAnime) {
-    console.log('app event', event);
+    const updates = [ ...event.willWatch, ...event.wonWatch ];
+    console.log('updates', updates);
+    this.queryForm.patchValue({ updates });
+    this.queryForm.updateValueAndValidity();
+    console.log(this.queryForm.get('updates')?.errors);
   }
 
 }
